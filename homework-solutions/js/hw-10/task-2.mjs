ea108c437812708c7775f1685bb09de58e372791
SoloweyAnd <soloweykoandrey@gmail.com>
2025-03-17 09:58:09 +0300

    HM 10

--- homework-solutions/js/hw-10/task-2.mjs
+++ homework-solutions/js/hw-10/task-2.mjs
@@ -7,8 +7,13 @@
   Ожидается: { 1: 1, 2: 2, 3: 1, 4: 3, 5: 1 }
 */
 
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
 function countOccurrences(arr) {
-  // ваш код
+  let result = arr.reduce((acc, num) => {
+    acc[num] ? (acc[num] += 1) : (acc[num] = 1);
+    return acc;
+  }, {});
+  return result;
 }
-
+console.log(countOccurrences(numbers));
 export { countOccurrences };
